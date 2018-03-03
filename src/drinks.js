3be5bf92df03fc1ae68e26b1d9c4a6b93c5e899e
Dalton Voth <dvoth@vervocity.io>
2018-03-03 15:18:58 -0600

    Updated drink styles

--- src/drinks.js
+++ src/drinks.js
@@ -2,12 +2,4 @@ $(".beer-std-drink").on('click', function() {
 	$("input[name=amount]").val(12);
 });
 
-$(".wine-wine-drink").on('click', function() {
-	$("input[name=amount]").val(5);
-});
-
-$(".liquor-wine-drink").on('click', function() {
-	$("input[name=amount]").val(1.5);
-});
-
 //function calcStandardDrinks(percentage, )
