27a3b19343dd34834ad5f4b92c4879be6d837f3e
jplute <indyplute@gmail.com>
2018-03-03 14:08:39 -0600

    Google Foo and You

--- dist/bacChart.php
+++ dist/bacChart.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
 <html>
     <header>
-
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="../src/css/data.css">
     </header>
     <body>
 
-        <canvas id="myChart" width="50" height="50"></canvas>
+        <div class="chartWrapper">
+            <div class="chartAreaWrapper">
+                <canvas id="myChart" class="w3-display-topmiddle"></canvas>
+            </div>
+        </div>
 
     </body>
 
     <footer> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="../src/backend.js"> </script>
         <script src="../node_modules/chart.js/dist/Chart.bundle.min.js"></script>
         <script src="../src/bacChart.js"></script>
