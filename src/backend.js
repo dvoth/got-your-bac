d669506f19cdd20c5f4a1ed1a4802370b5b10ea0
doshner <dro1895@gmail.com>
2018-03-03 08:14:42 -0600

    Code added
    added User class and began working on calculateBAC

--- src/backend.js
+++ src/backend.js
@@ -1,5 +1,23 @@
-var backend = {
-    calculateBAC: function(){
-        
+class user{
+    constructor(age, weight, BAClevel, gender){
+        this.age = age;
+        this.weight = weight;
+        this.BAClevel = BAClevel;
+        this.gender = gender; 
+    }
+
+}
+class backend{
+    calculateBAC(user){
+        var genderConstant; 
+        if(user.gender = "male"){
+            genderConstant = 0.68;
+        }else if(user.gender = "female"){
+            genderConstant = 0.55;
+        }
+    }
+    
+    updateBAC(){
+    
     }
 }
