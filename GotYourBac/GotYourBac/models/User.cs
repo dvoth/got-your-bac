12557904d605b61c0cb9079b15a752e31347b2ae
Dalton Voth <dvoth@vervocity.io>
2018-03-02 21:38:36 -0600

    Initial Commit

---
+++ GotYourBac/GotYourBac/models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotYourBac.models
+{
+    class User
+    {
+        private int age;
+        private double height;
+        private double weight;
+        private char gender;
+
+        User(int age, double height, double weight, char gender)
+        {
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+            this.gender = gender;
+        }
+
+        public int Age { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public char Gender { get; set; }
+
+    }
+}
