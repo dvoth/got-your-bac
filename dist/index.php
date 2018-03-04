0fa6ad3dfd9a5cc2810b361309974a883efb8a22
jtetzne <jtetzne@siue.edu>
2018-03-04 01:58:31 -0600

    Adding the Party features

--- dist/index.php
+++ dist/index.php
@@ -232,6 +232,10 @@ include('config.php');
                 <i class="fas fa-times"></i>
             </button>
             <div id="party_registration" class="w3-container">
+              
+            <button id = "createP">Create Party</button>
+            <div id="userList"></div>  
+               
             </div>
         </div>
         <button id="partybtn" class="w3-button w3-green w3-xlarge" onclick="party_open()">
