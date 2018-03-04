c522475253cf74dddac88c1dd5c7d2915ec3d77f
Dalton Voth <dvoth@vervocity.io>
2018-03-04 01:01:32 -0600

    Added jquery ajax

--- src/barGraph.js
+++ src/barGraph.js
@@ -13,37 +13,14 @@ $(document).ready(function () {
     var t6 = time +6;
 
     var testdata;
-    var xmlhttp = new XMLHttpRequest();
 
-    // $.ajax({
-    //     url:'api/drinksPerUnit.php',
-    //     type:'post',
-    //     data:'query=select price from tshirts.prices where size = "' + size + '"',
-    //     success:function(data){
-    //         if(isjson(data)){
-    //             data = $.parseJSON(data);
-    //             data = data[0];
-    //             price = data['price'];
-    //             //  continue code for calculation
-    //             //  this alert will display "0", but I want the price from the database in there
-    //             alert(price);
-    //            //perhaps do other ajax calls for other bits of data
-    //             //...
-    //         }else{
-    //             //display error getting data
-    //         }
-    //     }
-    // });
-    
-    xmlhttp.open("GET", "api/drinksPerUnit.php?timeframe=month&id=1", true);
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                testdata = JSON.parse(this.responseText);
-            }
-        };
-    }
+    $.ajax({
+        url:'api/drinksPerUnit.php?timeframe=month&id=1',
+        type:'get',
+        success:function(data){
+            console.log(data)
+        }
+    });
     
     console.log(testdata)
                     
